2a35a4b8c79ae7c9f28b7295c8ad8bca214fe3e7
Shah Neil Khan <iamsnkonline@gmail.com>
2025-05-24 03:06:01 -0700

    urls.dart

--- lib/data/utils/urls.dart
+++ lib/data/utils/urls.dart
@@ -1,33 +1,25 @@
 class Urls {
-  static final _baseUrl = 'http://35.73.30.144:2005/api/v1';
-  static final registerUrl = '$_baseUrl/Registration';
-  static final logInrUrl = '$_baseUrl/Login';
-  static final updateProfileUrl = '$_baseUrl/ProfileUpdate';
-  static final profileDetailsUrl = '$_baseUrl/ProfileDetails';
-  static final taskStatusCountUrl = '$_baseUrl/taskStatusCount';
-  static final createTaskUrl = '$_baseUrl/createTask';
-  static final resetPasswordRrl = '$_baseUrl/RecoverResetPassword';
+  static const String _baseUrl = 'http://35.73.30.144:2005/api/v1';
+  static const String registration = '$_baseUrl/registration';
+  static const String login = '$_baseUrl/login';
+  static const String addNewTask = '$_baseUrl/createTask';
+  static const String newTaskList = '$_baseUrl/listTaskByStatus/New';
+  static const String completedTaskList = '$_baseUrl/listTaskByStatus/Completed';
+  static const String taskStatusCount = '$_baseUrl/taskStatusCount';
+  static const String updateProfile = '$_baseUrl/profileUpdate';
+  static const String cancelledTaskList = '$_baseUrl/listTaskByStatus/Cancelled';
+  static const String progressTaskList = '$_baseUrl/listTaskByStatus/Progress';
 
+  static String changeStatus(String taskId, String status) =>
+      '$_baseUrl/updateTaskStatus/$taskId/$status';
 
-  static getTaskUrl(status) {
-    return '$_baseUrl/listTaskByStatus/$status';
-  }
-
-  static pswEmailVerify(email) {
-    return '$_baseUrl/RecoverVerifyEmail/$email';
-  }
-
-  static deleteTaskUrl(id) {
-    return '$_baseUrl/deleteTask/$id';
-  }
-
-  static forgetPasswordEmailVerifyUrl(email) {
-    return '$_baseUrl/RecoverVerifyEmail/$email';
-  }
-
-  static forgetPasswordEmailAndOPTVerify({email, otp}) {
-    return '$_baseUrl/RecoverVerifyOtp/$email/$otp';
-  }
-
+  static String deleteTask(String taskId) =>
+      '$_baseUrl/deleteTask/$taskId';
+  static String emailVerify(String email) =>
+      '$_baseUrl/RecoverVerifyEmail/$email';
+  static String recoverOTPVerify(String email,String code) =>
+      '$_baseUrl/RecoverVerifyOTP/$email/$code';
+  static String resetPassword() =>
+      '$_baseUrl/RecoverResetPassword';
 
 }
