9eb21cc3f61274ad926d60e400a4377e76cfa7f3
Shah Neil Khan <iamsnkonline@gmail.com>
2025-05-24 03:04:47 -0700

    task_controller.dart

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,32 +1,30 @@
 import 'package:get/get.dart';
-import '../services/api_service.dart';
-import '../models/task_model.dart';
+import 'package:tm_getx/ui/controllers/complete_task_controller.dart';
+import 'package:tm_getx/ui/controllers/forgot_email_controller.dart';
+import 'package:tm_getx/ui/controllers/forgot_otp_controller.dart';
+import 'package:tm_getx/ui/controllers/new_task_list_controller.dart';
+import 'package:tm_getx/ui/controllers/profile_controller.dart';
+import 'package:tm_getx/ui/controllers/progress_task_controller.dart';
+import 'package:tm_getx/ui/controllers/reset_password_controller.dart';
+import 'package:tm_getx/ui/controllers/sign_in_controller.dart';
+import 'package:tm_getx/ui/controllers/sign_up_controller.dart';
 
-class TaskController extends GetxController {
-  var tasks = <Task>[].obs;
-  ApiService apiService = ApiService();
+import 'ui/controllers/add_new_task_controller.dart';
 
+class ControllerBinder extends Bindings{
   @override
-  void onInit() {
-    fetchTasks();
-    super.onInit();
+  void dependencies() {
+    Get.put(SignInController());
+    Get.put(AddNewTaskController());
+    Get.put(SignUpController());
+    Get.put(ResetPasswordController());
+    Get.put(ProgressTaskController());
+    Get.put(ProfileController());
+    Get.put(ForgotOtpController());
+    Get.put(CompleteTaskController());
+    Get.put(ForgotEmailController());
+    Get.put(NewTaskListController());
   }
 
-  void fetchTasks() async {
-    try {
-      var fetchedTasks = await apiService.fetchTasks();
-      tasks.assignAll(fetchedTasks);
-    } catch (e) {
-      print('Error fetching tasks: $e');
-    }
-  }
 
-  void deleteTask(String id) async {
-    try {
-      await apiService.deleteTask(id);
-      fetchTasks();
-    } catch (e) {
-      print('Error deleting task: $e');
-    }
-  }
 }
