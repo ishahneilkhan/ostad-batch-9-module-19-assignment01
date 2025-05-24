9e0f3ce094b32bf75702af16d1d33ddbe4c6673a
Shah Neil Khan <iamsnkonline@gmail.com>
2025-05-24 03:05:13 -0700

    app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,70 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager_task/ui/screens/add_new_task_screen.dart';
-import 'package:task_manager_task/ui/screens/forget_password_email_verify_screen.dart';
-import 'package:task_manager_task/ui/screens/forget_password_otp_verification_screen.dart';
-import 'package:task_manager_task/ui/screens/login_screen.dart';
-import 'package:task_manager_task/ui/screens/main_bottom_nav_screen.dart';
-import 'package:task_manager_task/ui/screens/register_screen.dart';
-import 'package:task_manager_task/ui/screens/reset_password_screen.dart';
-import 'package:task_manager_task/ui/screens/splash_screen.dart';
-import 'package:task_manager_task/ui/screens/update_profile_screen.dart';
-class TaskManager extends StatelessWidget {
-  const TaskManager({super.key});
+import 'package:get/get.dart';
+import 'package:tm_getx/controller_binder.dart';
+import 'package:tm_getx/ui/screens/main_bottom_nav_bar_screen.dart';
+import 'package:tm_getx/ui/screens/reset_password_screen.dart';
+import 'package:tm_getx/ui/screens/sign_in_screen.dart';
+import 'package:tm_getx/ui/screens/splash_screen.dart';
+import 'package:tm_getx/ui/utils/app_colors.dart';
 
-  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+class TaskManagerApp extends StatefulWidget {
+  const TaskManagerApp({super.key});
+
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: TaskManager.navigatorKey,
-      title: 'Task Manager',
-      initialRoute:'/splash' ,
-      routes: {
-        '/splash': (context) => SplashScreen(),
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
-        '/resetPassword': (context) => ResetPasswordScreen(),
-        '/forgetPasswordEmail': (context) => ForgetPasswordEmailVerifyScreen(),
-        '/forgetPasswordPin': (context) => ForgetPasswordOtpVerificationScreen(),
-        '/MainBottomNavScreen': (context)=> MainBottomNavScreen(),
-        '/AddNewTaskScreen': (context) =>AddNewTaskScreen(),
-        '/UpdateProfileScreen': (context)=> UpdateProfileScreen(),
-
-      },
-      theme: ThemeData(
-        colorSchemeSeed: Colors.green,
-        textTheme: TextTheme(
-          labelLarge: TextStyle(color: Colors.grey),
-          headlineMedium: TextStyle(fontWeight: FontWeight.w600),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(fontWeight: FontWeight.w400, color: Colors.grey),
-          fillColor: Colors.white,
-          filled: true,
-          contentPadding: EdgeInsets.symmetric(horizontal: 16),
-          border: _getZeroBorder(),
-          enabledBorder: _getZeroBorder(),
-          errorBorder: _getZeroBorder(),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            fixedSize: const Size.fromWidth(double.maxFinite),
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.blue,
-            iconColor: Colors.white,
-            iconSize: 30,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
-          ),
-        ),
-      ),
-      // home: SplashScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
 }
 
-_getZeroBorder() {
-  return const OutlineInputBorder(borderSide: BorderSide.none);
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      navigatorKey: TaskManagerApp.navigatorKey,
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.themeColor,
+        textTheme: const TextTheme(),
+        inputDecorationTheme: _inputDecorationTheme(),
+        elevatedButtonTheme: _elevatedButtonThemeData(),
+      ),
+      initialBinding: ControllerBinder(),
+      initialRoute: '/',
+      routes: {
+        SplashScreen.name: (context) => const SplashScreen(),
+        MainBottomNavBarScreen.name: (context) =>
+            const MainBottomNavBarScreen(),
+        SignInScreen.name: (context) => const SignInScreen(),
+      },
+    );
+  }
+
+  ElevatedButtonThemeData _elevatedButtonThemeData() {
+    return ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.themeColor,
+        foregroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        fixedSize: const Size.fromWidth(double.maxFinite),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+
+  InputDecorationTheme _inputDecorationTheme() {
+    return InputDecorationTheme(
+      fillColor: Colors.white,
+      filled: true,
+      hintStyle: const TextStyle(fontWeight: FontWeight.w300),
+      border: _inputBorder(),
+      enabledBorder: _inputBorder(),
+      errorBorder: _inputBorder(),
+      focusedBorder: _inputBorder(),
+    );
+  }
+
+  OutlineInputBorder _inputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.circular(8),
+    );
+  }
 }
