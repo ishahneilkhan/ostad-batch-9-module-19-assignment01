f985bb2d7b594b5e19cdf597e1ae874d6e9fb653
Shah Neil Khan <iamsnkonline@gmail.com>
2025-05-24 02:39:11 -0700

    widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:task_manager_task/main.dart';
+import 'package:tm_getx/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
